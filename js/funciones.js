0decd57d930a80169a610d73d1067fcbe41c95ea
Lucho-MZ <luisalberto.mz@outlook.com>
2023-10-30 13:46:47 -0300

    2do commit

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,10 @@
 alert("Hola esto es un mensaje externo");
+/*let usuario = prompt("nombre")
+let contraseña = nombre(prompt("contraseña"))
+ if ((usuario === "lucho")&&(contraseña === "123")){
+    console.log( "bienvenido")
+ }
+ else{
+    console.log("U o C INCORRECTO")
+ }*/
+ 
